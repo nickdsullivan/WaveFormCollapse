9af155327de7c7bad8f8e7dccd0190fe991e6680
nickdsullivan <nickdsullivan@gmail.com>
2022-04-29 13:10:59 -0500

    test2

--- wfc/src/wfc/Grid.java
+++ wfc/src/wfc/Grid.java
@@ -1,8 +1,18 @@
 package wfc;
 
 public class Grid {
-	int 
+	Tile [] [] tiles;
+	int [] a;
 	public Grid(int w, int h) {
+		tiles = new Tile[w][h];
+		//init tiles
+		for (int col =0; col < tiles.length; col ++) {
+			for (int row = 0; row < tiles[col].length; row++) {
+				tiles[col][row] = new Tile();
+			}
+		}
+		
+		
 		
 	}
 }
